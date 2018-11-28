a214d99292a4210692a6496f3cb79adddcdee0f1
Tiffanatic <madsrynord@live.dk>
2018-11-28 14:34:57 +0100

    Implementation af login
    Fuldendt implementation af login

---
+++ RaunstrupAuth/Models/AboutModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace RaunstrupAuth.Models
+{
+    [Authorize]
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Your app description page";
+        }
+    }
+}
