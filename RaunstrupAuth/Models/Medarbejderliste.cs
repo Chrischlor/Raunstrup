a337b78b061aa340328b1cc1a7ac6c181b8c9c90
Tiffanatic <madsrynord@live.dk>
2018-11-29 23:35:49 +0100

    Update Medarbejderliste.cs

--- RaunstrupAuth/Models/Medarbejderliste.cs
+++ RaunstrupAuth/Models/Medarbejderliste.cs
@@ -8,10 +8,10 @@ namespace RaunstrupAuth.Models
     {
         [Key]
         public int Mlid { get; set; }
-        public int? Tid { get; set; }
-        public int? Mid { get; set; }
-        public int? Timer { get; set; }
-        public int? Rid { get; set; }
+        public int Tid { get; set; }
+        public int Mid { get; set; }
+        public int Timer { get; set; }
+        public int Rid { get; set; }
 
         public Medarbejder M { get; set; }
         public Rabat R { get; set; }
