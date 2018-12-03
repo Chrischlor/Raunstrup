99de4268fdb5582479cb22a3419b052888d2e47d
Tiffanatic <madsrynord@live.dk>
2018-12-03 10:54:44 +0100

    Update Medarbejder.cs
    opdatering af medarbejder.cs og integration af DB ændring

--- RaunstrupAuth/Models/Medarbejder.cs
+++ RaunstrupAuth/Models/Medarbejder.cs
@@ -13,13 +13,16 @@ namespace RaunstrupAuth.Models
         [Key]
         public int MID { get; set; }
         public string Navn { get; set; }
-        public int? Aid { get; set; }
-        public int? Tlf { get; set; }
+        public int Aid { get; set; }
+        public int Tlf { get; set; }
         public string Udd { get; set; }
-        public bool? Fudd { get; set; }
+        public bool Fudd { get; set; }
+        public int SpecialeID { get; set; }
+
 
         public Adresse A { get; set; }
         public ICollection<Medarbejderliste> Medarbejderliste { get; set; }
+        public Speciale S { get; set; }
         
     }
 }
