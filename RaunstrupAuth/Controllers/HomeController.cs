29dbf6ac98f35f711c95e08a6921a5c3ca92b722
Rune Brøgger Jepsen <rune4977@edu.eal.dk>
2018-12-04 14:08:56 +0100

    Rune

--- RaunstrupAuth/Controllers/HomeController.cs
+++ RaunstrupAuth/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RaunstrupAuth.Models;
@@ -10,6 +11,7 @@ namespace RaunstrupAuth.Controllers
 {
     public class HomeController : Controller
     {
+        
         public IActionResult Index()
         {
             return View();
@@ -27,8 +29,29 @@ namespace RaunstrupAuth.Controllers
             ViewData["Message"] = "Your contact page.";
 
             return View();
+            OnPost();
+            
         }
+        public async Task OnPost()
+        {
+            Email e = new Email();
+            using (var smtp = new SmtpClient())
+            {
+                smtp.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+                smtp.PickupDirectoryLocation = "Runebjepsen@gmail.com";
+                var msg = new MailMessage
+                {
+                    Body = e.Besked,
+                    Subject = e.Emne,
+                    From = new MailAddress(e.Mail)
 
+                };
+                msg.To.Add(e.Navn);
+                await smtp.SendMailAsync(msg);
+
+
+            }
+        }
         public IActionResult Privacy()
         {
             return View();
@@ -40,4 +63,5 @@ namespace RaunstrupAuth.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
+  
 }
