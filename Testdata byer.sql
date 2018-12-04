8f003567508456d800355eb45a71cb57b3a9a69f
Tiffanatic <madsrynord@live.dk>
2018-12-04 12:03:15 +0100

    Create Testdata byer.sql
    testdata

---
+++ Testdata byer.sql
@@ -0,0 +1,29 @@
+﻿--TESTDATA
+
+INSERT INTO dbo.Bynavn
+(
+    Postnummer,
+    Navn
+)
+VALUES
+(   1, -- Postnummer - int
+    'Copenhagen' -- Navn - varchar(50)
+    )
+INSERT INTO dbo.Bynavn
+(
+    Postnummer,
+    Navn
+)
+VALUES
+(   2, -- Postnummer - int
+    'Odense' -- Navn - varchar(50)
+    )
+INSERT INTO dbo.Bynavn
+(
+    Postnummer,
+    Navn
+)
+VALUES
+(   3, -- Postnummer - int
+    'Århus' -- Navn - varchar(50)
+)
