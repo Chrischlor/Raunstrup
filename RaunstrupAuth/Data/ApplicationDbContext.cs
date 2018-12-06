a885495026ee22dfe23676ab0f30bb9be4b5fd38
Tiffanatic <madsrynord@live.dk>
2018-12-06 10:40:32 +0100

    Update ApplicationDbContext.cs

--- RaunstrupAuth/Data/ApplicationDbContext.cs
+++ RaunstrupAuth/Data/ApplicationDbContext.cs
@@ -13,9 +13,33 @@ namespace RaunstrupAuth.Data
             : base(options)
         {
         }
+        
+        
+        public DbSet<Adresse> Adresse { get; set; }
+        public DbSet<Bynavn> Bynavn { get; set; }
+        public DbSet<Indkøbsliste> Indkøbsliste { get; set; }
         public DbSet<Kunde> Kunde { get; set; }
         public DbSet<Materialer> Materialer { get; set; }
-        public DbSet<Adresse> Adresse { get; set; }
-        public DbSet<RaunstrupAuth.Models.Bynavn> Bynavn { get; set; }
+        public DbSet<Medarbejder> Medarbejder { get; set; }
+        public DbSet<Medarbejderliste> Medarbejderliste { get; set; }
+        public DbSet<Projekt> Projekt { get; set; }
+        public DbSet<Rabat> Rabat { get; set; }
+        public DbSet<Speciale> Speciale { get; set; }
+        public DbSet<Tilbud> Tilbud { get; set; }
+
+        public override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Adresse>().ToTable("Adresse");
+            modelBuilder.Entity<Bynavn>().ToTable("Bynavn");
+            modelBuilder.Entity<Indkøbsliste>().ToTable("Indkøbsliste");
+            modelBuilder.Entity<Kunde>().ToTable("Kunde");
+            modelBuilder.Entity<Materialer>().ToTable("Materialer");
+            modelBuilder.Entity<Medarbejder>().ToTable("Medarbejder");
+            modelBuilder.Entity<Medarbejderliste>().ToTable("Medarbejderliste");
+            modelBuilder.Entity<Projekt>().ToTable("Projekt");
+            modelBuilder.Entity<Rabat>().ToTable("Rabat");
+            modelBuilder.Entity<Speciale>().ToTable("Speciale");
+            modelBuilder.Entity<Tilbud>().ToTable("Tilbud");
+        }
     }
 }
