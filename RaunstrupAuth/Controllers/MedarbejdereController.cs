db5076a9e03fbe28e10ee629adce53059f13e753
Tiffanatic <madsrynord@live.dk>
2018-12-10 12:38:43 +0100

    Opsætning af Medarbejdere

---
+++ RaunstrupAuth/Controllers/MedarbejdereController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RaunstrupAuth.Data;
+using RaunstrupAuth.Models;
+
+namespace RaunstrupAuth.Controllers
+{
+    public class MedarbejdereController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MedarbejdereController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Medarbejdere
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Medarbejder.Include(m => m.A).Include(m => m.Sp);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Medarbejdere/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var medarbejder = await _context.Medarbejder
+                .Include(m => m.A)
+                .Include(m => m.Sp)
+                .FirstOrDefaultAsync(m => m.Mid == id);
+            if (medarbejder == null)
+            {
+                return NotFound();
+            }
+
+            return View(medarbejder);
+        }
+
+        // GET: Medarbejdere/Create
+        public IActionResult Create()
+        {
+            ViewData["Aid"] = new SelectList(_context.Adresse, "Aid", "Aid");
+            ViewData["Spid"] = new SelectList(_context.Speciale, "Spid", "Spid");
+            return View();
+        }
+
+        // POST: Medarbejdere/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Mid,Navn,Aid,Tlf,Udd,Fudd,Spid")] Medarbejder medarbejder)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(medarbejder);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Aid"] = new SelectList(_context.Adresse, "Aid", "Aid", medarbejder.Aid);
+            ViewData["Spid"] = new SelectList(_context.Speciale, "Spid", "Spid", medarbejder.Spid);
+            return View(medarbejder);
+        }
+
+        // GET: Medarbejdere/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var medarbejder = await _context.Medarbejder.FindAsync(id);
+            if (medarbejder == null)
+            {
+                return NotFound();
+            }
+            ViewData["Aid"] = new SelectList(_context.Adresse, "Aid", "Aid", medarbejder.Aid);
+            ViewData["Spid"] = new SelectList(_context.Speciale, "Spid", "Spid", medarbejder.Spid);
+            return View(medarbejder);
+        }
+
+        // POST: Medarbejdere/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Mid,Navn,Aid,Tlf,Udd,Fudd,Spid")] Medarbejder medarbejder)
+        {
+            if (id != medarbejder.Mid)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(medarbejder);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MedarbejderExists(medarbejder.Mid))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Aid"] = new SelectList(_context.Adresse, "Aid", "Aid", medarbejder.Aid);
+            ViewData["Spid"] = new SelectList(_context.Speciale, "Spid", "Spid", medarbejder.Spid);
+            return View(medarbejder);
+        }
+
+        // GET: Medarbejdere/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var medarbejder = await _context.Medarbejder
+                .Include(m => m.A)
+                .Include(m => m.Sp)
+                .FirstOrDefaultAsync(m => m.Mid == id);
+            if (medarbejder == null)
+            {
+                return NotFound();
+            }
+
+            return View(medarbejder);
+        }
+
+        // POST: Medarbejdere/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var medarbejder = await _context.Medarbejder.FindAsync(id);
+            _context.Medarbejder.Remove(medarbejder);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MedarbejderExists(int id)
+        {
+            return _context.Medarbejder.Any(e => e.Mid == id);
+        }
+    }
+}
