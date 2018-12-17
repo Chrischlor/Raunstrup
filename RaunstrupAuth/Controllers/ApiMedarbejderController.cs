0fe32ed56b1f3e3e4c2205341e5897a6a95ca7b6
Yusuf Yücel Akdag <yusu0166@edu.eal.dk>
2018-12-17 14:36:31 +0100

    Udskrivning af medarbejder og Tilbud(Færdig)

--- RaunstrupAuth/Controllers/ApiMedarbejderController.cs
+++ RaunstrupAuth/Controllers/ApiMedarbejderController.cs
@@ -13,69 +13,37 @@ using DinkToPdf.EventDefinitions;
 
 namespace RaunstrupAuth.Controllers
 {
-    ////api/apimedarbejder
-    //[Route("api/[controller]")]
+    //api/apimedarbejder
     [Route("api/[controller]")]
     [ApiController]
     public class ApiMedarbejderController : Controller
     {
 
-        public ApplicationDbContext _context;
-        public IConverter _converter;
+        private ApplicationDbContext _context;
+        private IConverter _converter;
 
         //Dependency injection
         public ApiMedarbejderController(ApplicationDbContext context, IConverter converter)
         {
             _context = context;
             _converter = converter;
-            //Hvis medarbejder ikke eksisterer, så tilføj navn
-            if (_context.Medarbejder.Count() == 0)
-            {
-                _context.Medarbejder.Add(new Medarbejder { Navn = "Item2" });
-                _context.SaveChanges();
-            }
-        }
-        //Returnerer medarbejderne i json format
-        [Route("api/Getall")]
-        [HttpGet]
-        public ActionResult<List<Medarbejder>> GetAll()
-        {
-            return _context.Medarbejder.ToList();
-        }
-        //Søger efter Den pågældende medarbejders id
-        [HttpGet("{id}")]
-        public ActionResult<Medarbejder> GetById(int id)
-        {
-            var item = _context.Medarbejder.Find(id);
-
-            if (item == null)
-            {
-                return NotFound();
-            }
-            return item;
+            
+           
         }
 
-        
-
-     
         //Her bliver Pdf genereret
         [HttpGet]
         public ActionResult CreatePDF()
         {
 
-      
-
-            
+         //Konfiguration af pdf fil
         var globalSettings = new GlobalSettings
             {
                 ColorMode = ColorMode.Color,
                 Orientation = Orientation.Portrait,
                 PaperSize = PaperKind.A4,
                 Margins = new MarginSettings { Top = 10 },
-                DocumentTitle = "PDF Report",
-                //Out = @"C:\Users\Edb\Documents\PDFTEST\test1.pdf"
-
-
+                DocumentTitle = "PDF Report"
             };
 
 
@@ -83,20 +51,15 @@ namespace RaunstrupAuth.Controllers
 
             {
                 PagesCount = true,
-                HtmlContent = pdftemplate.Gethtmlstring(_context),
-                WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "assets", "styles.css") },
+                HtmlContent = pdfMedarbejder.Gethtmlstring(_context),
+                WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot","css", "styles.css" ) },
                 HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
                 FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = "Report Footer" }
             };
 
 
-           
-
-
-
-
-
-
+          
+            //HtmlToPdfDocument klassen bliver initialiseret, hvor der bliver lavet konfiguration af pdf fil. 
             var pdf = new HtmlToPdfDocument()
 
              {
@@ -107,11 +70,11 @@ namespace RaunstrupAuth.Controllers
 
              };
 
-           
 
-            var file= _converter.Convert(pdf);
-
-            return File(file, "application/pdf", "test.pdf" );
+            // Imens filen bliver konverteret, så bliver der også skabt et byte array, som bliver opbevaret i file variablen 
+            var file = _converter.Convert(pdf);
+            //returnerer pdf filen i browseren
+            return File(file, "application/pdf", "medarbejder.pdf" );
 
                }
             }
